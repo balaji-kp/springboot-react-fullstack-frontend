ac1e842fa618ec5c2bd439a891530ea2db253109
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-01-26 18:10:14 +0530

    Update EditUser.js

--- src/users/EditUser.js
+++ src/users/EditUser.js
@@ -25,12 +25,12 @@ export default function EditUser() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.put(`http://localhost:8080/user/${id}`, user);
+    await axios.put(`/user/${id}`, user);
     navigate("/");
   };
 
   const loadUser = async () => {
-    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    const result = await axios.get(`/user/${id}`);
     setUser(result.data);
   };
 
