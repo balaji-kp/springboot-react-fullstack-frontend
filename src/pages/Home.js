9ad91aa5b4d11401e7ba66da10c10861dbf54d27
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-01-26 17:43:06 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 
-axios.defatults.baseURL = 'http://3.111.215.66:8080/';
+
 export default function Home() {
   const [users, setUsers] = useState([]);
 
